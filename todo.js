dc7173b7654afe3b65a512c94c8eb6692c1471e0
hamza42076 <164366638+hamza42076@users.noreply.github.com>
2024-03-30 23:55:58 +0500

    todo

---
+++ todo.js
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "what you want to add in todos list? ",
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            Message: "Do you want to add more ",
+            default: "false"
+        },
+    ]);
+    todos.push(addTask.todo);
+    condition = addTask.addMore;
+    console.log(todos);
+}
+;
+let w = 1;
+w = "string";
+console.log(w);
